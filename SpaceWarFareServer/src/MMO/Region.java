7c2e9f08f59092d6d972717ac6980cb6294f7d93
Suyash Mohan <suyash.mohan@shephertz.co.in>
2014-03-10 17:39:13 +0530

    Source Code is now commented

--- SpaceWarFareServer/src/MMO/Region.java
+++ SpaceWarFareServer/src/MMO/Region.java
@@ -8,10 +8,20 @@ import java.util.List;
  * @author Suyash Mohan
  */
 public class Region {
+    //m_bound represents the rectangle that form the Rectangle inside the world
     private Rect m_bound;
+    //m_subscribers is the list of all the player who have subscribed to the region
     private List<Player> m_subscribers;
+    //m_players is the list of all player that are inside the region
     private List<Player> m_players;
     
+    /*
+     * Initialises the Region
+     * @param x         x-coordinate of position
+     * @param y         y-coordinate of position
+     * @param width     width of the Region
+     * @paaram height   height of the Region
+     */
     public Region(float x, float y, float width, float height)
     {
         m_bound = new Rect();
@@ -24,40 +34,65 @@ public class Region {
         m_players = new ArrayList<Player>();
     }
     
+    /*
+     * Adds a subscriber to the region
+     * A subscriber may not present inside the region
+     * Subscriber is the one who wants to listen to the region
+     * @param p Player that has to be added
+     */
     public void AddSubscriber(Player p)
     {
         if(m_subscribers.contains(p) == false)
             m_subscribers.add(p);
     }
     
+    /*
+     * Unsubscribe a player from the region by removing him 
+     * @param p Player that has to be removed from subscribtion
+     */
     public void RemoveSubscriber(Player p)
     {
         if(m_subscribers.contains(p) == true)
             m_subscribers.remove(p);
     }
     
+    /*
+     * Add a player inside the region
+     * @param p Player that has to be added
+     */
     public void AddPlayer(Player p)
     {
         if(m_players.contains(p) == false)
             m_players.add(p);
     }
     
+    /*
+     * Remove the player from the region
+     * @param p Player that has to be removed
+     */
     public void RemovePlayer(Player p)
     {
         if(m_players.contains(p) == true)
             m_players.remove(p);
     }
     
+    /*
+     * Returns the list of all Players
+     */
     public List<Player> getPlayers()
     {
         return m_players;
     }
     
+    /*
+     * Returns the list of all the subscribers
+     */
     public List<Player> getSubscribers()
     {
         return m_subscribers;
     }
     
+    //Returns the rectangle representing the region
     public Rect getRect()
     {
         return m_bound;
--- SpaceWarFareServer/src/MMO/World.java
+++ SpaceWarFareServer/src/MMO/World.java
@@ -11,14 +11,28 @@ import java.util.List;
  * @author Suyash Mohan
  */
 public class World {
+    //Reference to IRoom
     private IRoom m_room;
+    //2D List of all regions
     private Region m_regions[][];
+    //Map of IUser and Player
     private HashMap<IUser,Player> m_players;
+    //TopLeft Corner of World
     private VectorF m_tl;
+    //BottomRight Corner of World
     private VectorF m_br;
+    //Number of regions the world is divided into
     private VectorI m_subdivisions;
+    //Size of one division
     private VectorF m_divisionArea;
     
+    /*
+     * Initialises the World
+     * @param room          IRoom to which the world is attached to
+     * @param TopLeft       This represents the coordinates of Top Left point
+     * @param BottomRight   This represents the coordinates of Bottom Right point
+     * @param SubDivisions  This is the nummber of regions the world has to be divided into
+     */
     public World(IRoom room,VectorF TopLeft, VectorF BottomRight, VectorI SubDivisions)
     {
         m_room = room;
@@ -46,6 +60,11 @@ public class World {
         }
     }
     
+    /*
+     * Returns a region that contains the (x,y) point
+     * @param x
+     * @param y
+     */
     public Region findRegion(float x, float y)
     {
         int i = (int)Math.floor((x - m_tl.x)/m_divisionArea.x);
@@ -54,12 +73,20 @@ public class World {
         return m_regions[i][j];
     }
     
+    /*
+     * Adds a player to the world
+     * @param p Player that is being added to world
+     */
     public void addPlayer(Player p)
     {
         m_players.put(p.getIUser(), p);
         updatePlayer(p,null,null,null);
     } 
     
+    /*
+     * Removes a player from the world
+     * @param p Player that has to be removed
+     */
     public void removePlayer(Player p)
     {
         try
@@ -81,6 +108,10 @@ public class World {
         }
     }
     
+    /*
+     * Get the player reference from the IUser reference
+     * @param user IUser reference to the user
+     */
     public Player getPlayer(IUser user)
     {
         if(m_players.containsKey(user) == true)
@@ -89,6 +120,14 @@ public class World {
         return null;
     }
     
+    /*
+     * Updates the player instance in the world
+     * @param p                 Player that has to be updated in the world
+     * @param removedForPlayers Reference to the list, which will contain the list of all players from whom's interest area the Player p has been removed
+     * @param removedPlayer     Reference to the list, which will contain the list of all player that has left the player p's interest area
+     * @param addedPlayer       Reference to the list, which will contain the list of all player that has entered the player p's interest area
+     * @return void
+     */
     public void updatePlayer(Player p, List<Player> removedForPlayers, List<Player> removedPlayers, List<Player> addedPlayers)
     {
         try
@@ -139,6 +178,10 @@ public class World {
         }
     }
     
+    /*
+     * Updates the list of region that fall in the player's area of interest
+     * @param p     Player 
+     */
     private void updateInterestRegions(Player p)
     {
         VectorF aoi = p.getInterestAreaSize();
@@ -165,6 +208,11 @@ public class World {
         }
     }
     
+    /*
+     * Send Message to all the players subscribed to the Player p's current region
+     * @param p     Player who is sending the message
+     * @param msg   Message to be sent
+     */
     public void multicastMessage(Player p, String msg)
     {
         IUser user = p.getIUser();
@@ -177,6 +225,13 @@ public class World {
         }
     }
     
+    /*
+     * Send a messaged to a list of players
+     * @param sender        Player who is sending the message
+     * @param receivers     List of players who will be receiving the message
+     * @param msg           Message to be sent
+     * @return void
+     */
     public void multicastMessage(Player sender, List<Player> receivers, String msg)
     {
         IUser user = sender.getIUser();
@@ -186,6 +241,12 @@ public class World {
         }
     }
     
+    /*
+     * Send a message to a player
+     * @param receiver      Player who will recieve the message
+     * @param message       Message to be sent
+     * @param sender        Player who is sending the message
+     */
     public void sendMessage(Player receiver, String message, Player sender)
     {
         if(sender == null)
@@ -194,6 +255,9 @@ public class World {
         receiver.getIUser().SendChatNotification(sender.getIUser().getName(), message, m_room);
     }
     
+    /*
+     * Returns the list of players who has left the region rgn 
+     */
     private List<Player> removedFromRegion(List<Player> players, Region rgn)
     {
         List<Player> removedPlayers = new ArrayList<>();
